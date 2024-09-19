1ec60860542849aa99a36a88c4d7653b135b472c
ArKloK <ccamachobanez@gmail.com>
2024-09-19 12:09:26 +0200

    Project 01 - Twitter Follow Card

--- Projects/01-twitter-cards/src/App.jsx
+++ Projects/01-twitter-cards/src/App.jsx
@@ -2,16 +2,54 @@ import "./App.css";
 import { TwitterFollowCard } from "./TwitterFollowCard";
 
 
-const format = (userName) => `@${userName}`
+const users = [
+    {
+        userName: "ccamacho_00",
+        name: "Carlos",
+        isFollowing: false
+    },
+    {
+        userName: "midudev",
+        name: "Midudev",
+        isFollowing: true
+    }
+]
 
-
+//APP definition with map
 export function App() {
 
     return (
         <section className="App">
-            <TwitterFollowCard formatUserName={format} userName="ccamacho_00" name="Carlos" />
-            <TwitterFollowCard formatUserName={format} userName="midudev" name="Carlos" />
+            {
+                users.map((user) => {
+                    const { userName, name, isFollowing } = user;
+                    return (
+                        <TwitterFollowCard
+                            key={userName}
+                            userName={userName}
+                            initialIsFollowing={isFollowing}>
+                            {name}
+                        </TwitterFollowCard>
+                    );
+                })
+            }
         </section>
     )
 
 }
+
+//Single APP definition
+/*export function App() {
+
+    return (
+        <section className="App">
+            <TwitterFollowCard userName="ccamacho_00" >
+                Carlos
+            </TwitterFollowCard>
+            <TwitterFollowCard userName="midudev" initialIsFollowingisFollowing>
+                Midudev
+            </TwitterFollowCard>
+        </section>
+    )
+
+}*/
--- Projects/01-twitter-cards/src/TwitterFollowCard.jsx
+++ Projects/01-twitter-cards/src/TwitterFollowCard.jsx
@@ -1,32 +1,35 @@
 import { useState } from "react"
 
-export function TwitterFollowCard({userName ="none", name}) {
-    
-    const [isFollowing, setIsFollowing] = useState(false)
+export function TwitterFollowCard({ children, userName = "none", initialIsFollowing = false }) {
+
+    const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
 
     const handleClick = () => {
         setIsFollowing(!isFollowing)
     }
 
     const followText = isFollowing ? "Siguiendo" : "Seguir"
-    const followButtonClass = isFollowing ? 
-    "tw-follow-card-button is-following" :
-    "tw-follow-card-button"
+    const followButtonClass = isFollowing ?
+        "tw-follow-card-button is-following" :
+        "tw-follow-card-button"
 
     return (
         <article className="tw-follow-card">
             <header className="tw-follow-card-header">
                 <img
                     className="tw-follow-card-avatar"
-                    src={`https://unavatar.io/${userName}`} 
+                    src={`https://unavatar.io/${userName}`}
                     alt="Avatar de twitter" />
                 <div className="tw-follow-card-info">
-                    <strong className="tw-follow-card-infoName">{name}</strong>
+                    <strong className="tw-follow-card-infoName">{children}</strong>
                     <span className="tw-follow-card-infoUserName">@{userName}</span>
                 </div>
             </header>
             <aside>
-                <button className={followButtonClass} onClick={handleClick}>{followText}</button>
+                <button className={followButtonClass} onClick={handleClick}>
+                    <span className="tw-followCard-text">{followText}</span>
+                    <span className="tw-followCard-stopFollow">Dejar de seguir</span>
+                    </button>
             </aside>
         </article>
     )
